a316af96aa5ea7a88ff3fe006998dbb2fcf2088d
rdejaen <rdejaen@gmail.com>
2016-12-06 08:59:57 -0800

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -7,7 +7,7 @@ import java.io.*;
  * Reason for class: Plays the In Order game by getting a char array
  * and printing and checking the user's response.
  *
- * @version 06/20/2016
+ * @version 12/06/2016
  
  * @author Rachel De Jaen
  */
@@ -39,28 +39,22 @@ public class InOrder {
       return chars;
    }
    
+   public ArrayList<String> getWordArray() throws FileNotFoundException {
+      ArrayList<String> words = new ArrayList<String>();  
+      Scanner scanWords = new Scanner(new File(inputFile));
+      for (int k = 0; scanWords.hasNext(); k++) {    
+         String w = scanWords.next();
+         words.add(k, w);
+      }
+      return words;
+   }
 
-//*****************************************************************************************************************************//   
-//    public ArrayList<String> getWordArray(Scanner scanner) {
-//       ArrayList<String> words = new ArrayList<String>();        
-//    //       for (int k = 0; scanner.hasNext(); k++) { //causes the same exception if I do the while loop
-//    //          String single = scanner.next();
-//    //          words.set(k, single);
-//    //       }
-//       int k = 1;
-//       while (scanner.hasNext()) {
-//          String w = scanner.next();
-//          words.set(k, w); //causing the index out of bounds exception
-//          k++;
-//       }
-//       return words;
-//    }
-//*****************************************************************************************************************************//
-
-   public void printAndCheck (Scanner console, Scanner scanner, String decision, int count, char[] chars, Student user) throws FileNotFoundException { //prints the prompt and gets the users response, checks response with the correct answer
+   public void printAndCheck (Scanner console, String decision, int count, Student user) throws FileNotFoundException { //prints the prompt and gets the users response, checks response with the correct answer
       String inputFile = getInputFile();
-            
+      Scanner scanner = new Scanner (new File (inputFile));
+      decision = user.getDecision();     
       if (decision.equals("character")) {  //if the user wants to memorize a file of characters
+         char[] chars = getCharArray(scanner); //nothing is getting stored in here?
          String response = console.nextLine();
          char resp = response.charAt(0);
          char check = chars[count];
@@ -76,34 +70,51 @@ public class InOrder {
             user.incrTotal(1);
             user.stats(); //prints continuous statistics (rolling after every game)
          }
-         else{ //if the user's response was not correct
+         else if (resp != check && count == chars.length - 1) {
+            user.incrTotal(1);
+            System.out.println("That is incorrect. The correct answer was " + "'" + check + "'");
+            System.out.println("Your answer was " +  "'" + response + "'");
+            System.out.println("Nice job! You finished the game!");
+         }
+         else if (resp != check && count != chars.length - 1) { //if the user's response was not correct
             user.incrTotal(1);
             System.out.println("That is incorrect. The correct answer was " + "'" + check + "'");
             System.out.println("Your answer was " +  "'" + response + "'");
             System.out.println("What is the next character?");
          }                                    
       }
-   }
-//*****************************************************************************************************************************//
-//       else if (decision.equals("word")) { //sequence of words    
-//          ArrayList<String> words = getWordArray(scanner);     
-//          System.out.print(words.get(0));
-//          for (int m = 1; m <= words.size() - 2; m++) {
-//             boolean isCorrectB = true;
-//             while (isCorrectB) {
-//                String responseB = console.nextLine();
-//                if (responseB.equalsIgnoreCase(words.get(count + 1))) {
-//                   isCorrectB = true;
-//                }
-//                else {
-//                  isCorrectB = false;
-//                   System.out.println("That is incorrect. The correct answer was " + words.get(count + 1).toString());
-//                   System.out.println("Your answer was " + responseB);
-//                }
-//            }
-//         }
-//       }
-//*****************************************************************************************************************************//
-   
+      else if (decision.equals("word")) { //sequence of words    
+         ArrayList<String> words = getWordArray();    
+         String r = console.nextLine();
+         String check = words.get(count);
+         if (r.equals(check) && count != words.size() - 1) { 
+            System.out.println("That was correct! What is the next word?");
+            user.incrNumCorrect();
+            user.incrTotal(1);
+         }
+         else if (r.equals(check) && count == words.size() - 1) {
+            System.out.println("That was correct! ");
+            System.out.println("Nice job! You finished the game! ");
+            user.incrNumCorrect();
+            user.incrTotal(1);
+            user.stats();
+         }
+         else if (!r.equals(check) && count != words.size() - 1) {
+            user.incrTotal(1);
+            System.out.println("That is incorrect. The correct answer was " + "\"" + check + "\"");
+            System.out.println("Your answer was " +  "\"" + r + "\"");
+            System.out.println("What is the next word?");
+         }
+         else if (!r.equals(check) && count == words.size() - 1) {
+            user.incrTotal(1);
+            System.out.println("That is incorrect. The correct answer was " + "\"" + check + "\"");
+            System.out.println("Your answer was " +  "\"" + r + "\"");
+            System.out.println("Nice job! You finished the game! ");
+            user.incrTotal(1);
+            user.stats();
+         }
+      
+      }   
+   }  
    
 }
