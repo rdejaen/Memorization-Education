0e343147a305627bff8e0a751d3fd1e9223334d4
rdejaen <rdejaen@gmail.com>
2017-01-12 09:28:25 -0800

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -59,18 +59,18 @@ public class InOrder {
       //String sum = "";
       if (decision.equals("character")) {  //if the user wants to memorize a file of characters
          char[] chars = getCharArray(scanner);
-         if (count == 1) {
-            sum = chars[0] + "";
-         }
-         else{
-            sum += chars[count];
-         }
+//          if (count == 1) {
+//             sum = chars[0] + "";
+//          }
+//          else{
+//             sum += chars[count];
+//          }
 
          char[] chars = getCharArray(scanner); //nothing is getting stored in here?
          String response = console.nextLine();
          char resp = response.charAt(0);
          char check = chars[count];
-       
+         sum += check;
       if (resp == (check)) { //if the user's reponse was correct and it's not at the end of the file
             //sum += chars[count];////////////
 
@@ -100,6 +100,7 @@ public class InOrder {
                //still need to add this stuff to git hub copy
                //String b = new String (chars);
                //c = b.substring(0, b.indexOf(check) + 1);
+               sum = getCharString(scanner).substring(0, chars[count + 1]);
                System.out.println("What is after '" + sum + "' ?");
                
                //make it print the entire thing previous, and not just the last character
