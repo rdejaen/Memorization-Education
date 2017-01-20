36587f0fcc82bf4fa6e66a7ae02bfd37a584ec3a
Rachel De Jaen <rdejaen@gmail.com>
2017-01-20 09:25:42 -0800

    test

--- MemorizationEducation/MemorizationClientClass.java
+++ MemorizationEducation/MemorizationClientClass.java
@@ -13,6 +13,7 @@ import java.io.*;
  */
 ///////////////test to check integration//////////////////////////////
 ////////////////second commit test/////////
+////////////////test//////////
 public class MemorizationClientClass {
    public static void main (String [] args) throws FileNotFoundException { //prints the beginning introduction and calls the other intro methods (intro and intro2)
                                                                            //most methods return something, which is stored in the Student object (named user)
